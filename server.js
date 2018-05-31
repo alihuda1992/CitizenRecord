b86973e10e31359c726f39bbec5eddf08432a762
Ali Huda <alihuda1992@gmail.com>
2018-05-31 15:58:27 -0400

    updating

--- server.js
+++ server.js
@@ -1,2 +1,46 @@
-const handlebars = require ("handlebars");
+var express = require("express");
+var bodyParser = require("body-parser");
 
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "day_planner_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Use Handlebars to render the main index.html 
+// app.get("/", function(req, res) {
+//   connection.query("SELECT * FROM plans;", function(err, data) {
+//     if (err) {
+//       return res.status(500).end();
+//     }
+
+//     res.render("index", { plans: data });
+//   });
+// });
