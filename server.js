bb1f62c650d25807e1be2579fc9753c7244d55e1
Madinah2491 <madinah2491@gmail.com>
2018-05-31 20:58:18 -0400

    added authentication info & auth routes

--- server.js
+++ server.js
@@ -1,28 +1,138 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+// var dotenv = require('dotenv');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
+var flash = require('connect-flash');
 var app = express();
+
+// dotenv.load();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+var routes = require('./routes/index');
+var user = require('./routes/user');
+
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var exphbs = require("express-handlebars");
+// This will configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'shhhhhhhhh',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+
+// Handle auth failure error messages
+app.use(function(req, res, next) {
+  if (req && req.query && req.query.error) {
+    req.flash("error", req.query.error);
+  }
+  if (req && req.query && req.query.error_description) {
+    req.flash("error_description", req.query.error_description);
+  }
+  next();
+ });
+ 
+ // Check logged in
+ app.use(function(req, res, next) {
+   res.locals.loggedIn = false;
+   if (req.session.passport && typeof req.session.passport.user != 'undefined') {
+     res.locals.loggedIn = true;
+   }
+   next();
+ });
+ 
+ app.use('/', routes);
+ app.use('/user', user);
+ 
+ // catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+   const err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+ });
+
+ if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "day_planner_db"
+  password: "Maddough2018!",
+  database: "citizenrecord"
 });
 
 connection.connect(function(err) {
