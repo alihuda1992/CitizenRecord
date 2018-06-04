468db0031a9683a77e9060c1d59fcc588b79aad8
Ali Huda <alihuda1992@gmail.com>
2018-06-04 16:42:49 -0400

    insanity

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   AUTH0_CALLBACK_URL:
-  process.env.AUTH0_CALLBACK_URL || '/user'
+  process.env.AUTH0_CALLBACK_URL || '/'
 };
 
 /* GET home page. */
