ebad5d36118a88f5664b40acf420cd104f858e43
Ali Huda <alihuda1992@gmail.com>
2018-06-05 17:19:45 -0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   AUTH0_CALLBACK_URL:
-  process.env.AUTH0_CALLBACK_URL || 'https://citizenrecord2.herokuapp.com/callback'
+  process.env.AUTH0_CALLBACK_URL || 'https://citizenrecord2.herokuapp.com/userpage'
 };
 
 /* GET home page. */
@@ -31,7 +31,7 @@ router.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-router.get('/callback',
+router.get('/userpage',
   passport.authenticate('auth0', {
     failureRedirect: '/failure'
   }),
