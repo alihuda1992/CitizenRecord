a9f3f6099047396df5d361d6b737ea7bcdba1a1f
Ali Huda <alihuda1992@gmail.com>
2018-06-05 11:15:03 -0400

    ugh

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   AUTH0_CALLBACK_URL:
-  process.env.AUTH0_CALLBACK_URL || 'https://localhost:8080/callback'
+  process.env.AUTH0_CALLBACK_URL || 'https://localhost:3030/callback'
 };
 
 /* GET home page. */
